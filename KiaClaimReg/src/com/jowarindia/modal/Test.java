3867194e434814931c59b2c657aa165e5023d47a
Pratikshahegade <pratikshahegade0305@gmail.com>
2021-12-02 17:23:24 +0530

    New print statement

--- KiaClaimReg/src/com/jowarindia/modal/Test.java
+++ KiaClaimReg/src/com/jowarindia/modal/Test.java
@@ -3,5 +3,6 @@ package com.jowarindia.modal;
 public class Test {
     public static void main(String[] args) {
 		System.out.println("Pratiksha here");
+		System.out.println("Pratiksha here");
 	}
 }
